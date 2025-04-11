93d4f36662cf10302db2442340825046491f71f7
shuvomahamud <shuvomahamud@gmail.com>
2025-04-11 00:15:32 -0400

    Ran Identity migration to add Identity tables to Supabase

---
+++ Infrastructure/DashboardDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Infrastructure; // Make sure this is here!
+
+namespace Infrastructure
+{
+    public class DashboardDbContextFactory : IDesignTimeDbContextFactory<DashboardDbContext>
+    {
+        public DashboardDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<DashboardDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseNpgsql(connectionString);
+
+            return new DashboardDbContext(builder.Options);
+        }
+    }
+}
