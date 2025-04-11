5fa685eb80715cfb6b3fbf39d5fb706e9344b86a
shuvomahamud <shuvomahamud@gmail.com>
2025-04-10 23:33:47 -0400

    Installed required NuGet packages for EF Core (Npgsql, Tools, Design) and Identity

---
+++ Infrastructure/DashboardDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.IdentityModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Infrastructure
+{
+    public class DashboardDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DashboardDbContext(DbContextOptions<DashboardDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Optionally add DbSet placeholders for your application tables here.
+        // They will be included after scaffolding later.
+        // public DbSet<InterviewInformation> InterviewInformations { get; set; }
+        // public DbSet<AccountsPayable> AccountsPayables { get; set; }
+        // public DbSet<ToDoTask> ToDoTasks { get; set; }
+        // public DbSet<Onboarding> Onboardings { get; set; }
+    }
+}
