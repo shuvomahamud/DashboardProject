aa29c10017e2204337bdcf8d3ddf4df789c9fcbc
shuvo mahamud <shuvomahamud@gmail.com>
2025-07-14 15:50:03 -0400

    Vercel error fix

--- scripts/create-admin-user.js
+++ scripts/create-admin-user.js
@@ -1,5 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { randomUUID } = require('crypto');
 
 const prisma = new PrismaClient();
